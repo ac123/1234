8a32a71b496aca2a9b1fc94154296d069cf8b5af
adam c <adamscheslow@gmail.com>
2017-10-16 23:16:11 -0400

    time for something fresh

--- projects/lrthw/ex1.rb
+++
@@ -1,8 +0,0 @@
-#puts "Hello World!"
-#puts "Hello Again"
-#puts "I like typing this."
-#puts "this is fun"
-#puts "Yay! Printing"
-#puts "I'd Rather you 'not'."
-#puts 'I "said" do not touch this.'
-puts "the lotion on the skin."
--- projects/lrthw/ex2.rb
+++
@@ -1,4 +0,0 @@
-# a comment
-puts "I could have code like this>"
-
-puts "oh yeah" #but not ohhhhh yeah
